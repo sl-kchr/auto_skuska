6ee6fdefd8edad3a4e46a0644e66b90c7ee68758
sl-kchr <kucherslavka@gmail.com>
2024-10-11 11:22:03 +0200

    v_4

---  chat_gpt_1.py
+++  chat_gpt_1.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-p = Path('HW_2.txt')
+p = Path('HW.txt')
 p.write_text('60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5')
 
 def get_cats_info(path):
