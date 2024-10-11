39c8586d31d424bb98c7aeba780bf72f879d3e26
sl-kchr <kucherslavka@gmail.com>
2024-10-11 16:15:45 +0200

    v

---
+++ du4/ex_4_test.py
@@ -0,0 +1,48 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return 'Contact added'
+
+def change_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return 'Contact updated.' 
+
+def show_phone(args, contacts):
+    name = args
+    phone = contacts[name]
+    return phone
+
+def show_all(contacts):
+    return(contacts)
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command == 'close' or command == 'exit':
+            print('Good bye!')
+            break
+        elif command == 'hello':
+            print("How can I help you?")
+        elif command == 'add':
+            print(add_contact(args, contacts))
+        elif command == 'change':
+            print(change_contact(args, contacts))
+        elif command == 'phone':
+            print(show_phone(args, contacts))
+        elif command == 'all':
+            print(show_all(contacts))
+        else:
+            print('Invalid command.')
+
+if __name__ == '__main__':
+    main()
